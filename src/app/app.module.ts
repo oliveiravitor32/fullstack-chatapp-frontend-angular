71dd7d158acdd97b2e757ad710f5a884a8c6a745
oliveiravitor32 <oliveiravitor327@gmail.com>
2023-12-18 14:54:55 -0300

    Try: optimize background images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { LoginRegisterModule } from './views/login-register/login-register.module';
-import { ChatModule } from './views/chat/chat.module';
-import { ChatRoomsModule } from './views/chat-rooms/chat-rooms.module';
 
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    LoginRegisterModule,
-    ChatRoomsModule,
-    NoopAnimationsModule,
-    ChatModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, NoopAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
