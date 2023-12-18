e9a52713fae7df3784052c954e8509e04bc2d709
oliveiravitor32 <oliveiravitor327@gmail.com>
2023-12-18 11:47:15 -0300

    Delete: login-form register-form

--- src/app/views/login-register/auth-form/auth-form.component.ts
+++ src/app/views/login-register/auth-form/auth-form.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-auth-form',
   templateUrl: './auth-form.component.html',
-  styleUrls: ['./auth-form.component.css']
+  styleUrls: ['./auth-form.component.css'],
 })
 export class AuthFormComponent {
-
+  nickname = new FormControl('');
+  password = new FormControl('');
 }
--- src/app/views/login-register/login-register.module.ts
+++ src/app/views/login-register/login-register.module.ts
@@ -2,20 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoginRegisterRoutingModule } from './login-register-routing.module';
-import { FormLoginComponent } from './form-login/form-login.component';
-import { FormRegisterComponent } from './form-register/form-register.component';
 import { LoginRegisterComponent } from './login-register.component';
 import { AuthFormComponent } from './auth-form/auth-form.component';
 import { SharedMaterialModule } from 'src/app/shared/shared-material.module';
 
 @NgModule({
-  declarations: [
-    FormLoginComponent,
-    FormRegisterComponent,
-    LoginRegisterComponent,
-    AuthFormComponent,
-  ],
-  exports: [LoginRegisterComponent, FormLoginComponent, LoginRegisterComponent],
+  declarations: [LoginRegisterComponent, AuthFormComponent],
+  exports: [LoginRegisterComponent, AuthFormComponent],
   imports: [CommonModule, LoginRegisterRoutingModule, SharedMaterialModule],
 })
 export class LoginRegisterModule {}
