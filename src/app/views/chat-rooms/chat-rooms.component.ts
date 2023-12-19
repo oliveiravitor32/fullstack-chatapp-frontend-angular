b849ed41dfe7c0845500f5e5d62d72c82b9aae84
oliveiravitor32 <oliveiravitor327@gmail.com>
2023-12-19 15:22:46 -0300

    Add: Hostlistener, debounce Time and Subject (rxjs) to dynamically change the side bar display in chat-rooms view

--- src/app/views/chat-rooms/chat-rooms.component.ts
+++ src/app/views/chat-rooms/chat-rooms.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { debounceTime } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-chat-rooms',
   templateUrl: './chat-rooms.component.html',
-  styleUrls: ['./chat-rooms.component.css']
+  styleUrls: ['./chat-rooms.component.css'],
 })
-export class ChatRoomsComponent {
+export class ChatRoomsComponent implements OnInit {
+  private resizeSubject = new Subject();
+  private resizeSubscription;
 
+  desktopWindow: boolean = true;
+
+  constructor() {
+    this.resizeSubscription = this.resizeSubject
+      .pipe(debounceTime(500))
+      .subscribe(() => {
+        this.detectDesktopScreenSize();
+      });
+  }
+
+  ngOnInit(): void {
+    this.detectDesktopScreenSize();
+  }
+
+  detectDesktopScreenSize() {
+    const width = window.innerWidth;
+    if (width <= 800) {
+      this.desktopWindow = false;
+    } else {
+      this.desktopWindow = true;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.resizeSubject.next(event);
+  }
+
+  ngOnDestroy() {
+    this.resizeSubscription.unsubscribe();
+  }
 }
