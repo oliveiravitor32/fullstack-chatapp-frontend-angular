bc8b6d941a1ab16c747743bbc91c75f90f0b39d8
oliveiravitor32 <oliveiravitor327@gmail.com>
2023-12-22 11:30:53 -0300

    Create: responsivity in chat view

--- src/app/views/chat/chat.component.ts
+++ src/app/views/chat/chat.component.ts
@@ -3,7 +3,7 @@ import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css'],
+  styleUrls: ['./chat.component.css', './chat.responsive.component.css'],
 })
 export class ChatComponent {
   message = new FormControl('');
