32c2690f73e189013fe7d54157c9819afd92cede
oliveiravitor32 <oliveiravitor327@gmail.com>
2023-12-22 14:29:02 -0300

    Create: navigation in side-bar-menu (inject dependecy - router)

--- src/app/components/side-bar-menu/side-bar-menu.component.ts
+++ src/app/components/side-bar-menu/side-bar-menu.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MenuControlService } from 'src/app/service/menu-control.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar-menu',
@@ -9,16 +10,36 @@ import { MenuControlService } from 'src/app/service/menu-control.service';
     './side-bar-menu.responsive.component.css',
   ],
 })
-export class SideBarMenuComponent {
+export class SideBarMenuComponent implements OnInit {
   statusMenu: boolean = false;
+  inChatRooms: boolean = false;
 
-  constructor(private menuControlService: MenuControlService) {
+  constructor(
+    private router: Router,
+    private menuControlService: MenuControlService,
+    private route: ActivatedRoute
+  ) {
     this.menuControlService.openSideBarMenu$.subscribe((state: boolean) => {
       this.statusMenu = state;
     });
   }
 
+  ngOnInit(): void {
+    const componentName = this.route.snapshot.component?.name;
+    if (componentName === 'ChatRoomsComponent') {
+      this.inChatRooms = true;
+    }
+  }
+
   toggleMenu() {
     this.menuControlService.openSideBarMenu();
+  }
+
+  navigateToChatRooms() {
+    this.router.navigate(['/chatrooms']);
+  }
+
+  navigateToChat() {
+    this.router.navigate(['/chatrooms/chat']);
   }
 }
