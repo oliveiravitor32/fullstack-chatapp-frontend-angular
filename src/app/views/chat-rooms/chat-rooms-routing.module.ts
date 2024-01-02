4e6b47b7e90075bc9116f2ce33fc6dbb81ff7c3b
oliveiravitor32 <oliveiravitor327@gmail.com>
2024-01-02 12:51:56 -0300

    Create: authGuard

--- src/app/views/chat-rooms/chat-rooms-routing.module.ts
+++ src/app/views/chat-rooms/chat-rooms-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChatRoomsComponent } from './chat-rooms.component';
+import { authGuard } from 'src/app/guards/auth.guard';
 
-const routes: Routes = [{ path: '', component: ChatRoomsComponent }];
+const routes: Routes = [
+  { path: '', component: ChatRoomsComponent, canActivate: [authGuard] },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/views/chat/chat-routing.module.ts
+++ src/app/views/chat/chat-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChatComponent } from './chat.component';
+import { authGuard } from 'src/app/guards/auth.guard';
 
-const routes: Routes = [{ path: '', component: ChatComponent }];
+const routes: Routes = [
+  { path: '', component: ChatComponent, canActivate: [authGuard] },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
